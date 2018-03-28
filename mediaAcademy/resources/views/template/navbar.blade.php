8a52cd0d0d3a66acb0e5aa9d4b5981c93ee092ed
Lars Voort <lvoort4891@student.landstede.nl>
2018-03-28 14:33:18 +0200

    Laravel project toegevoegd

---
+++ mediaAcademy/resources/views/template/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <a class="navbar-brand" href="#">
+        <img src="https://c.disquscdn.com/uploads/forums/330/7083/favicon.png" alt="logo"/>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav row">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Websites studenten</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link " href="#">Lars</a>
+            </li>
+        </ul>
+    </div>
+
+</nav>
