48bdb63a9ed02d8384a3a890ffe91cd5419929c3
Lars Voort <lvoort4891@student.landstede.nl>
2018-04-05 14:22:48 +0200

    banner

--- mediaAcademy/resources/views/template/template.blade.php
+++ mediaAcademy/resources/views/template/template.blade.php
@@ -13,9 +13,13 @@
         <!-- Styles -->
 
     </head>
-    <body class="container">
-       @include('template.navbar')
-@yield('content')
+    <body>
+        <div class="container">
+            @include('template.navbar')
+        </div>
+        @include('template.banner')
+
+        @yield('content')
 
 <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
     </body>
