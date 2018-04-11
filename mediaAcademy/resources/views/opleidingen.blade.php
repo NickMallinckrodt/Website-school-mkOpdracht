6d37fd91cdbb090671e6c2a0454060837ee64087
Stefan Peters <stefan.peters1999@hotmail.com>
2018-04-11 14:53:35 +0200

    tweede website update 11-4-2018

--- mediaAcademy/resources/views/opleidingen.blade.php
+++ mediaAcademy/resources/views/opleidingen.blade.php
@@ -8,8 +8,34 @@
 
   <div class="row">
       <div class="col">
+        <br>
           <h1>Applicatie- & Mediaontwikkelaar</h1>
-            <p></p>
+            <p>Heb jij een smartphone of tablet? Dan weet je hoe enorm handig een app kan zijn.
+               Teminste: als deze goed werkt en gemakkelijk in het gebruik is. Zou jij voor klanten een app willen bedenken én maken?</p>
+
+            <h3>Beroep</h3>
+            <p>Met de opleiding applicatie- & mediaontwikkelaar leer je apps van begin tot eind bouwen.
+              En om updates te doen aan bestaande.
+              Je laat apps samenwerken met bijvoorbeeld social media, met andere apparaten of software en je weet ze te koppelen aan bijvoorbeeld de webshopfunctie van een klant.
+              Dat is vaak een behoorlijke technische uitdaging.</p>
+
+            <p>Ook adviseer je over en werk je mee aan (mobiele) websites, frameworks, content management systemen (CMS)
+              en geef je advies over de beveiliging ervan. De ontwikkelingen in web, mobile en e-commerce gaan snel.
+              Dankzij jou groeien weboplossingen mee met deze trends. Na deze opleiding kun je aan de slag bij bijvoorbeeld
+              een media- of internetbedrijf of op een IT-afdeling. Je doet een groot deel van het werk zelfstandig, maar je
+              werkt ook vaak samen in teams met vormgevers en andere specialisten. Je bent vaak de vraagbaak voor marketing-
+              en communicatiespecialisten en opdrachtgevers. Dat vraagt ook om sociale skills en kennis van andere richtingen.</p>
+
+              <h3>Je sterke punten</h3>
+              <p>Natuurlijk moet je goed met computers en software kunnen omgaan, maar je moet meer in huis hebben.
+                Zo zijn klantgerichtheid en inlevingsvermogen erg belangrijk. Je moet je namelijk goed kunnen verplaatsen
+                in je opdrachtgever en de eindgebruiker. Gedurende je opleiding ligt er een zwaar accent op het samenwerken.
+                Uitgeverijen, communicatie- en reclamebureaus, internetondernemingen en drukkerijen zoeken
+                tegenwoordig applicatieontwikkelaars die in hun eigen vakgebied een kei zijn, maar vooral volop kunnen
+                ontwikkelen, uitwisselen en samenwerken met collega’s in verschillende disciplines.
+                Verder ben je een echte doorzetter. Want hoewel je technisch bijna alles kan, kom je tijdens de uitvoering
+                echt weleens problemen tegen die je niet één, twee, drie oplost. Tenslotte wordt ook van je verwacht dat
+                je conceptueel kunt denken.</p>
           <hr>
 
           <h1>Media Manager</h1>
